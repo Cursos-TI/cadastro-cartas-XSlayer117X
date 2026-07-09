2008efdda73b72b075a34f220b947506499e92f5
XSlayer117X <123337884+XSlayer117X@users.noreply.github.com>
2026-07-09 15:18:43 -0300

    Modficadoralaong.c
    Corrected data types and updated print format for long int.

--- Modficadoralaong.c
+++ Modficadoralaong.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int numeroNormal = 214748347; //Valor máximo de int
+    long long int numeroGrande = 2147483647;
+
+    printf("Número regular (int): %d\n", numeroNormal);
+    printf("Núimero grande (long int): %lld\n", numeroGrande);
+
+    numeroGrande = 2147483648; //Valor maior que o maximo de int
+    printf("Número grande atualizado (long int): %ld\n", numeroGrande);
+            return 0;
+
+}
